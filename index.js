9062a12cb476766c47a4d242d73c2004148f1f60
Chris Leary <learymchris@gmail.com>
2023-08-16 11:24:28 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const beans = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  
+  superbowlWin = (record) => {
+    let result = record.find( record => record.result === "W" )
+    console.log(result)
+    if (typeof result === 'undefined') return undefined
+    else return result.year
+  }
+
+console.log(superbowlWin(beans))
